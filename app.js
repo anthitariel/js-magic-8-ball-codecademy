4a65768a14f3d5f17c0e32a49052c2c095e05b4b
Anfisa <anfiya17@gmail.com>
2023-11-30 23:14:36 +0000

    Enhanced Magic 8 Ball look and functionality: - Styled the interface to resemble the Interstate 60 magic ball. - Added a digital-style font for the Magic 8 Ball answers. - Hid the number when displaying the result text. - Improved hover effects and button styles.

--- app.js
+++ app.js
@@ -5,20 +5,27 @@ const magicBallElement = document.getElementById('magic-ball-result');
 const questionElement = document.getElementById('question');
 const resultElement = document.getElementById('magic-ball-result');
 const userQuestionElement = document.getElementById('user-question');
+const magicWaitingText = 'Ah, it seems the magic is waiting for your question to weave its spell. Please enter a question, and let the enchantment begin!';
 
 magicBallElement.addEventListener('click', resetMagicBall);
 
 function getPrediction() {
     const userQuestion = questionElement.value.trim();
     if (!userQuestion) {
-        resultElement.innerText = 'Ah, it seems the magic is waiting for your question to weave its spell. Please enter a question, and let the enchantment begin!';
+        resultElement.innerText = magicWaitingText;
         userQuestionElement.innerText = '';
-        magicBallElement.classList.remove('reset');
+        magicBallElement.classList.remove('reset', 'show-number');
     } else {
         const randomNumber = Math.floor(Math.random() * 8);
-        const answers = ['It is certain', 'It is decidedly so', 'Reply hazy try again', 'Cannot predict now', 'Do not count on it', 'My sources say no', 'Outlook not so good', 'Signs point to yes'];
+        const answers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy, try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful'];
+        const magicBallResult = answers[randomNumber];
         userQuestionElement.innerText = `If you are really interested in "${userQuestion}" here is my answer for you below. Click on the magic ball if you want to ask another question.`;
-        resultElement.innerText = answers[randomNumber];
+        resultElement.innerText = magicBallResult;
+        if (magicBallResult === magicWaitingText) {
+            magicBallElement.classList.remove('show-number');
+        } else {
+            magicBallElement.classList.add('show-number');
+        }
         magicBallElement.classList.add('reset');
     }
 }
