221ba04481cad9bdca09416ec55fed13906daf78
Anfisa <anfiya17@gmail.com>
2023-11-30 18:30:35 +0000

    // Optimized JavaScript code for Magic Eight Ball project // Changes made for better readability and consistency // Consolidated getElementById calls, used descriptive variable names, and removed unnecessary assignments // Commit Message: "Optimize JavaScript code for Magic Eight Ball project"

--- app.js
+++ app.js
@@ -1,51 +1,31 @@
-/* Magic Eight Ball 
-https://www.codecademy.com/courses/introduction-to-javascript/projects/magic-eight-ball-1
-*/
+// Magic Eight Ball 
+// https://www.codecademy.com/courses/introduction-to-javascript/projects/magic-eight-ball-1
 
-let userName = '';
-userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
-const userQuestion = 'Will I become a superstar tonight?';
-console.log(userQuestion);
-const randomNumber = Math.floor(Math.random() * 8);
+const magicBallElement = document.getElementById('magic-ball-result');
+const questionElement = document.getElementById('question');
+const resultElement = document.getElementById('magic-ball-result');
+const userQuestionElement = document.getElementById('user-question');
 
+magicBallElement.addEventListener('click', resetMagicBall);
 
-// my solution 
-
-const answers = ['It is certain', 'It is decidedly so','Reply hazy try again','Cannot predict now','Do not count on it','My sources say no','Outlook not so good','Signs point to yes']
-console.log(answers[randomNumber]);
-
-  
-/*
-switch (randomNumber) {
-  case 0:
-    eightBall = 'It is certain';
-    break;
-  case 1:
-    eightBall = 'It is decidedly so';
-    break;  
-  case 2:
-    eightBall = 'Reply hazy try again';
-    break;
-  case 3:
-    eightBall = 'Cannot predict now';
-    break; 
-  case 4:
-    eightBall = 'Do not count on it';
-    break; 
-  case 5:
-    eightBall = 'My sources say no';
-    break; 
-  case 6:
-    eightBall = 'Reply hazy try again';
-    break; 
-  case 7:
-    eightBall = 'Outlook not so good';
-    break; 
-  case 8:
-    eightBall = 'Signs point to yes';
-    break; 
-  // additional cases...
+function getPrediction() {
+    const userQuestion = questionElement.value.trim();
+    if (!userQuestion) {
+        resultElement.innerText = 'Ah, it seems the magic is waiting for your question to weave its spell. Please enter a question, and let the enchantment begin!';
+        userQuestionElement.innerText = '';
+        magicBallElement.classList.remove('reset');
+    } else {
+        const randomNumber = Math.floor(Math.random() * 8);
+        const answers = ['It is certain', 'It is decidedly so', 'Reply hazy try again', 'Cannot predict now', 'Do not count on it', 'My sources say no', 'Outlook not so good', 'Signs point to yes'];
+        userQuestionElement.innerText = `If you are really interested in "${userQuestion}" here is my answer for you below. Click on the magic ball if you want to ask another question.`;
+        resultElement.innerText = answers[randomNumber];
+        magicBallElement.classList.add('reset');
+    }
 }
-*/
 
-// console.log(`The eight ball answered: ${eightBall}`)
+function resetMagicBall() {
+    resultElement.innerText = '';
+    userQuestionElement.innerText = '';
+    magicBallElement.classList.remove('reset');
+    questionElement.value = '';
+}
